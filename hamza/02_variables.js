5ecb6f2509bf5536fb3f90d7e8c578414d57b984
wajid19jxm <hamzakhanlalo1@gmail.com>
2025-03-24 17:28:23 +0000

    datatype

---
+++ hamza/02_variables.js
@@ -0,0 +1,21 @@
+"use strick" // treat all js code in newer version
+//tamam code ko new version ke roop shape ma istemal karein;
+//alert (2+2)// we are using nodejs; not browser;
+//code readability should be high
+
+
+let name = "hamza"//string
+let age = 18
+let isloggedIn = false
+let state;
+
+//number => 2  to the power 53
+//bigint
+//string ""
+//boolean => true/false
+//null => standloan value
+//undefined => undefined
+//symbol => its is a uneque data type
+//object
+console.log(typeof umdefined)
+console.log(null)
