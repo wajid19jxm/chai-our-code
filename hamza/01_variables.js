5182a1645e6cdd3a3ae3a42fd5079bc435ef540e
wajid19jxm <hamzakhanlalo1@gmail.com>
2025-03-24 17:29:21 +0000

    datatypes

---
+++ hamza/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = "14445"
+let accountEmail = "hamza@gmail.com" 
+var accountPassward = "123456"
+accountCity = "shalbandi"
+accountEmail = "zakir@gmail..com"
+accountPassward = "32132"
+accountCity = "kalpani"
+
+console.log("accountId");
+console.table([accountId, accountEmail, accountPassward, accountCity])
